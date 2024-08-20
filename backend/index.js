c66d4aeeca93fdea6d809b8289326bb9f272ab27
Nguyễn Thanh Vũ <thanhvuthtn@gmail.com>
2024-08-20 14:29:40 +0700

    b01

---
+++ backend/index.js
@@ -0,0 +1,7 @@
+//dotenv
+const dotenv = require('dotenv')
+dotenv.config()
+//-------------------------------------------------
+//express
+const express = require('express')
+const app = express()
